22a110564e233615929bb4af94fd04ed2e77656e
Daniel Garcia <daniel.garcia7913@gmail.com>
2021-07-18 11:48:42 -0400

    mongo uri added to heroku

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev') {
    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://daniel:garushia09@prueba1.izvsu.mongodb.net/cafe?authSource=admin&replicaSet=atlas-9289iw-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
